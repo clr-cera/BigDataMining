649d5d5e139fef4967e936a7b4bbf83da7bb4b37
FelipeCarneiroMachado <felipecmachado05@gmail.com>
2026-09-21 10:21:30 -0300

    Notebook pronto

--- projects/p2/p2.sql
+++ projects/p2/p2.sql
@@ -1,15 +1,15 @@
 -- standalone script
 
-DROP MATERIALIZED VIEW IF EXISTS ExamesColesterol CASCADE;
+DROP VIEW IF EXISTS ExamesColesterol CASCADE;
 DROP MATERIALIZED VIEW IF EXISTS ExamesColesterolDiff;
 
-CREATE MATERIALIZED VIEW ExamesColesterol AS (
+CREATE VIEW ExamesColesterol AS (
     SELECT P.id_paciente, E.dt_coleta, E.id_atendimento,
         MAX(D.de_desfecho) AS Desfecho, -- fiz essa
         MAX(E.de_hospital) AS Hospital,
-        MIN(D.dt_desfecho) - E.dt_coleta AS "Dias até desfecho", -- fiz essa
-        ROW_NUMBER() OVER (PARTITION BY P.id_paciente ORDER BY E.dt_coleta) "N° do exame de colesterol do paciente", -- fiz essa
-        ROW_NUMBER() OVER (PARTITION BY P.id_paciente, E.id_atendimento ORDER BY E.dt_coleta) "N° do exame de colesterol do atendimento", -- fiz essa
+        MAX(D.dt_desfecho) - E.dt_coleta AS "Dias até desfecho", -- fiz essa
+        DENSE_RANK() OVER (PARTITION BY P.id_paciente ORDER BY E.dt_coleta) "N° do exame de colesterol do paciente", -- fiz essa
+        DENSE_RANK() OVER (PARTITION BY P.id_paciente, E.id_atendimento ORDER BY E.dt_coleta) "N° do exame de colesterol do atendimento", -- fiz essa
         AVG(REPLACE(E.de_resultado, ',', '.')::FLOAT) FILTER(WHERE E.de_analito ~*'ldl' AND E.de_analito!~*'vldl') AS LDL,
         AVG(REPLACE(E.de_resultado, ',', '.')::FLOAT) FILTER(WHERE E.de_analito ~*'hdl') AS HDL,
         AVG(REPLACE(E.de_resultado, ',', '.')::FLOAT) FILTER(WHERE E.de_analito ~*'v.*coles')AS VLDL,
@@ -39,5 +39,4 @@ CREATE MATERIALIZED VIEW ExamesColesterolDiff AS (
 );
 
 SELECT * FROM ExamesColesterolDiff
-ORDER BY COUNT(*) OVER(PARTITION BY id_paciente) DESC, id_paciente, dt_coleta
-;
+ORDER BY COUNT(*) OVER(PARTITION BY id_paciente) DESC, id_paciente, dt_coleta;
